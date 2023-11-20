dd5e7aeb18583af5dc2dcad8b2258f6fa9d5b7ec
MD Nazirul Islam <139609914+webnpage@users.noreply.github.com>
2023-11-20 22:28:42 +0600

    Update script.js

--- script.js
+++ script.js
@@ -79,3 +79,90 @@ for(let i = 0; i < todoChildren.length; i++) {
 for(let i = 0; i < comChildren.length; i++) {
     bindCompleteItems(comChildren[i], deleteTask)
 }
+
+
+//adding date and time to web
+
+
+const Month = new Date().getMonth();
+let text1 = "" ;
+
+switch(Month) {
+  case 0:
+    text1 = "January";
+    break;
+  case 1:
+    text1 = "February";
+    break;
+  case 2:
+    text1 = "March";
+    break;
+  case 3:
+    text1 = "April";
+    break;
+  case 4:
+    text1 = "May";
+    break;
+  case 5:
+    text1 = "June";
+    break;
+  case 6:
+    text1 = "July";
+    break;
+  case 7:
+    text1 = "Auguest";
+    break;
+  case 8:
+    text1 = "September";
+    break;
+  case 9:
+    text1 = "October";
+    break;
+  case 10:
+    text1 = "November";
+    break;
+  case 11:
+    text1 = "December";
+    
+}
+
+const day = new Date().getDay();
+let text = "" ;
+
+switch(day) {
+  case 0:
+    text = "Sunday";
+    break;
+  case 1:
+    text = "Monday";
+    break;
+  case 2:
+    text = "Tuesday";
+    break;
+  case 3:
+    text = "Wednesday";
+    break;
+  case 4:
+    text = "Thrusday";
+    break;
+  case 5:
+    text = "Friday";
+    break;
+  case 6:
+    text = "Saturday";
+    
+}
+
+
+function getTimeIn12HourFormat() {
+  const now = new Date();
+  
+
+  const localTime = new Date().toLocaleTimeString();
+
+  const time12HourFormat = `<h1>${localTime}</h1> <h3>${text} ${now.getDate()} ${text1}  ${now.getFullYear()} </h3>`;
+    
+  document.getElementById('demo').innerHTML = time12HourFormat ;
+}
+
+setInterval(getTimeIn12HourFormat, 0)
